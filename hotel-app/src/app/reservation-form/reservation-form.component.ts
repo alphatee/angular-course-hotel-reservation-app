5ff87bedd3eb47560ae715973f9d721aa24cc1b8
Daniel Herrera <dher426@wgu.edu>
2025-03-09 23:59:45 -0700

    created a reservation form

--- hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
 
+  reservationForm: FormGroup = new FormGroup({});
+
+  /**
+   *
+   */
+  constructor() {
+    super();
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit() {
+    if (this.reservationForm.valid)
+      console.log("valid")
+  }
 }
